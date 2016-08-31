40defd4ded30726f44782710a29c3ac445b84211
Jaegeuk Kim <jaegeuk@kernel.org>
2016-08-31 14:04:57 -0700

    fix gather.sh
    Signed-off-by: Jaegeuk Kim <jaegeuk@kernel.org>

--- gather_results.sh
+++ gather_results.sh
@@ -16,7 +16,7 @@ rsync -r --delete-excluded --delete perf:/root/xfstests-f2fs/fs_log.txt fs_mark/
 echo "Info: copy fxmark/$1 from perf"
 rm -rf fxmark/$1 2>/dev/null
 mkdir fxmark/$1 2>/dev/null
-rsync -r --delete-excluded --delete perf:/root/fxmark/fxmark_nvme/* fxmark/$1/
+rsync -r --delete-excluded --delete perf:/root/fxmark-f2fs/fxmark_nvme/* fxmark/$1/
 
 echo "Info: copy phoronix/$1 from perf"
 rm -rf phoronix/$1 2>/dev/null
